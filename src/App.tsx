41dd6a2f395cd88948b69f27d08b3953bea5ef07
Esther <esthercardososilva@gmail.com>
2021-05-02 17:09:31 -0300

    Modifica Título do App no browser; - Corrige referência aos arquivos de estilo em App.tsx e index.tsx de   .css para .scss; - Adiciona referência à HeaderBar no App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import HeaderBar from './components/HeaderBar/HeaderBar';
 
 function App() {
   return (
     <div className="App">
+      <HeaderBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
